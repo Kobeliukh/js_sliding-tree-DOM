56208da02de110ac45b6ea1fdda5ecdbb0507235
Vladyslav <vkobeluh@gmail.com>
2025-12-06 14:48:49 +0200

    Add solution (attempt 1)

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,3 +1,35 @@
 'use strict';
 
-// write code here
+const tree = document.querySelector('.tree');
+const uls = tree.querySelectorAll('ul');
+
+tree.addEventListener('click', (e) => {
+  if (e.target.tagName !== 'SPAN') {
+    return;
+  }
+
+  const ul = e.target.nextElementSibling;
+  let state = 'none';
+
+  if (!ul) {
+    return;
+  }
+
+  if (ul.style.display === 'none') {
+    state = '';
+  }
+
+  ul.style.display = state;
+});
+
+uls.forEach((ul) => {
+  if (ul.previousSibling.nodeType !== 3) {
+    return;
+  }
+
+  const span = document.createElement('span');
+
+  span.textContent = ul.previousSibling.textContent.trim();
+
+  ul.previousSibling.replaceWith(span);
+});
